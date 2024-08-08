8b06dc0aba681153e313ae1118d78204787b6a72
Felix <matfelix@proton.me>
2024-08-07 22:15:10 -0300

    Changed  parameters names

--- aiwass.py
+++ aiwass.py
@@ -16,11 +16,11 @@ def load_texts():
         return json.load(f)
 
 # Função para pegar um verso de um livro, dado seu nome, versão, capítulo e versículo
-def get_verse(book_title, version, chapter, verse):
+def get_verse(book, version, chapter, verse):
     texts = load_texts()
-    book = texts.get(book_title)
-    if book and version in book['versions']:
-        return book['versions'][version]['verses'].get(str(chapter), {}).get(str(verse))
+    book_data = texts.get(book)
+    if book_data and version in book_data['versions']:
+        return book_data['versions'][version]['verses'].get(str(chapter), {}).get(str(verse))
     return None
 
 # Função para registrar inputs e outputs do usuário em um arquivo .log
@@ -40,34 +40,35 @@ async def on_ready():
     print(f'{bot.user} has connected to Discord!')
     await bot.tree.sync()  # sincroniza os comandos da árvore
 
+### Comandos
 # Comando para mostrar um verso específico, dado o livro, versão, capítulo e versículo
 @bot.tree.command(name='verse', description='Mostra um verso específico')
-async def get_verse_command(interaction: discord.Interaction, book_title: str, version: str, chapter: str, verse: int):
-    verse_text = get_verse(book_title, version, chapter, verse)
+async def get_verse_command(interaction: discord.Interaction, book: str, language: str, chapter: str, verse: int):
+    verse_text = get_verse(book, language, chapter, verse)
     if verse_text:
         output = f"*{verse_text}*"
         await interaction.response.send_message(output)
     else:
         output = 'Verso não encontrado.'
         await interaction.response.send_message(output)
-    log_interaction(interaction.user, 'verse', book_title, version, chapter, verse, output=output)  # Registra o output
+    log_interaction(interaction.user, 'verse', book, version, chapter, verse, output=output)  # Registra o output
 
 # Comando para mostrar um verso aleatório, dado o livro e a versão
 @bot.tree.command(name='random', description='Mostra um verso aleatório')
-async def random_verse(interaction: discord.Interaction, book_title: str, version: str):
+async def random_verse(interaction: discord.Interaction, book: str, language: str):
     texts = load_texts()
-    book = texts.get(book_title)
-    if book and version in book['versions']:
-        verses = book['versions'][version]['verses']
+    book_data = texts.get(book)
+    if book_data and language in book_data['versions']:
+        verses = book_data['versions'][language]['verses']
         chapter = random.choice(list(verses.keys()))
         verse = random.choice(list(verses[chapter].keys()))
         verse_text = verses[chapter][verse]
-        output = f"`{chapter}.{verse}`:\n*{verse_text}*"
+        output = f"`{chapter}.{verse}:`\n*{verse_text}*"
         await interaction.response.send_message(output)
     else:
         output = 'Livro ou versão não encontrada.'
         await interaction.response.send_message(output)
-    log_interaction(interaction.user, 'random', book_title, version, output=output)  # Registra o output
+    log_interaction(interaction.user, 'random', book, language, output=output)  # Registra o output
 
 # Comando para mostrar informações sobre todos os livros da database
 @bot.tree.command(name='books', description='Mostra informações sobre os livros disponíveis')
@@ -75,7 +76,7 @@ async def books(interaction: discord.Interaction):
     texts = load_texts()
     if texts:
         output = ""
-        for book_title, book_info in texts.items():
+        for book, book_info in texts.items():
             author = book_info.get('author', 'Desconhecido')
             code = book_info.get('code', 'Desconhecido')
             year = book_info.get('year', 'Desconhecido')
@@ -87,7 +88,7 @@ async def books(interaction: discord.Interaction):
                 if link:
                     links_str += f"[[{version}]({link})] "
 
-            output += f"## **{book_title}**\n`Código`: {code}\n`Autor`: {author}\n`Ano`: {year}\n`Descrição`: *{description}*\n`Versões`: {links_str}\n"
+            output += f"## **{book}**\n`Código:` {code}\n`Autor:` {author}\n`Ano:` {year}\n`Versões:` {links_str}\n`Descrição:` *{description}*\n"
         await interaction.response.send_message(output)
     else:
         output = 'Nenhum livro encontrado na base de dados.'
@@ -96,32 +97,32 @@ async def books(interaction: discord.Interaction):
 
 # Comando para mostrar versos que contenha o termo pesquisado
 @bot.tree.command(name='search', description='Pesquisa por versos')
-async def search_verses(interaction: discord.Interaction, book_title: str, version: str, keywords: str):
+async def search_verses(interaction: discord.Interaction, book: str, language: str, keywords: str):
     texts = load_texts()
-    if book_title not in texts:
-        output = f"O livro '{book_title}' não existe."
+    if book not in texts:
+        output = f"O livro '{book}' não existe."
         await interaction.response.send_message(output)
-        log_interaction(interaction.user, 'search', book_title, version, keywords, output=output)
+        log_interaction(interaction.user, 'search', book, language, keywords, output=output)
         return
 
-    book = texts[book_title]
-    if version not in book['versions']:
-        output = f"A versão '{version}' não existe para o livro '{book_title}'."
+    book_data = texts[book]
+    if language not in book_data['versions']:
+        output = f"A versão '{language}' não existe para o livro '{book}'."
         await interaction.response.send_message(output)
-        log_interaction(interaction.user, 'search', book_title, version, keywords, output=output)
+        log_interaction(interaction.user, 'search', book, language, keywords, output=output)
         return
 
-    verses = book['versions'][version]['verses']
+    verses = book_data['versions'][language]['verses']
     results = []
     count = 0  # Contador de aparições
     for chapter, chapter_verses in verses.items():
         for verse, text in chapter_verses.items():
             if all(keyword.lower() in text.lower() for keyword in keywords.split()):
-                results.append(f"`{chapter}.{verse}`:\n*{text}*")
+                results.append(f"`{chapter}.{verse}:`\n*{text}*")
                 count += 1  # Incrementa o contador
 
     if results:
-        response = f"**{count} ocorrência(s) de `{keywords}` em \"{book_title}\":**\n"
+        response = f"**{count} ocorrência(s) de `{keywords}` em \"{book}\":**\n"
         message_limit = 1900
         current_message = response
 
@@ -143,48 +144,48 @@ async def search_verses(interaction: discord.Interaction, book_title: str, versi
                 await interaction.followup.send(current_message)
         output = "Resultados enviados!"
     else:
-        output = f"Nenhum resultado encontrado para a pesquisa de {keywords} em \"{book_title}\"."
+        output = f"Nenhum resultado encontrado para a pesquisa de {keywords} em \"{book}\"."
         await interaction.response.send_message(output)
-    log_interaction(interaction.user, 'search', book_title, version, keywords, output=output)
+    log_interaction(interaction.user, 'search', book, version, keywords, output=output)
 
-# Autocompletes
-@random_verse.autocomplete('book_title')
-@get_verse_command.autocomplete('book_title')
-@search_verses.autocomplete('book_title')
-async def autocomplete_book_title(interaction: discord.Interaction, current: str) -> list[Choice]:
+### Autocompletes
+@random_verse.autocomplete('book')
+@get_verse_command.autocomplete('book')
+@search_verses.autocomplete('book')
+async def autocomplete_book(interaction: discord.Interaction, current: str) -> list[Choice]:
     texts = load_texts()
-    book_titles = [book_title for book_title in texts.keys() if current.lower() in book_title.lower()]
-    return [Choice(name=book_title, value=book_title) for book_title in book_titles]
+    books = [book for book in texts.keys() if current.lower() in book.lower()]
+    return [Choice(name=book, value=book) for book in books]
 
-@random_verse.autocomplete('version')
-@get_verse_command.autocomplete('version')
-@search_verses.autocomplete('version')
+@random_verse.autocomplete('language')
+@get_verse_command.autocomplete('language')
+@search_verses.autocomplete('language')
 async def autocomplete_version(interaction: discord.Interaction, current: str) -> list[Choice]:
     texts = load_texts()
-    book_title = next((option['value'] for option in interaction.data['options'] if option['name'] == 'book_title'), None)
-    if book_title and book_title in texts:
-        versions = texts[book_title]['versions'].keys()
-        return [Choice(name=version, value=version) for version in versions if current.lower() in version.lower()]
+    book = next((option['value'] for option in interaction.data['options'] if option['name'] == 'book'), None)
+    if book and book in texts:
+        languages = texts[book]['versions'].keys()
+        return [Choice(name=language, value=language) for language in languages if current.lower() in language.lower()]
     return []
 
 @get_verse_command.autocomplete('chapter')
 async def autocomplete_chapter(interaction: discord.Interaction, current: str) -> list[Choice]:
     texts = load_texts()
-    book_title = next((option['value'] for option in interaction.data['options'] if option['name'] == 'book_title'), None)
-    version = next((option['value'] for option in interaction.data['options'] if option['name'] == 'version'), None)
-    if book_title and version and book_title in texts and version in texts[book_title]['versions']:
-        chapters = texts[book_title]['versions'][version]['verses'].keys()
+    book = next((option['value'] for option in interaction.data['options'] if option['name'] == 'book'), None)
+    language = next((option['value'] for option in interaction.data['options'] if option['name'] == 'language'), None)
+    if book and language and book in texts and language in texts[book]['versions']:
+        chapters = texts[book]['versions'][language]['verses'].keys()
         return [Choice(name=chapter, value=str(chapter)) for chapter in chapters if str(current) in str(chapter)]
     return []
 
 @get_verse_command.autocomplete('verse')
 async def autocomplete_verse(interaction: discord.Interaction, current: str) -> list[Choice]:
     texts = load_texts()
-    book_title = next((option['value'] for option in interaction.data['options'] if option['name'] == 'book_title'), None)
-    version = next((option['value'] for option in interaction.data['options'] if option['name'] == 'version'), None)
+    book = next((option['value'] for option in interaction.data['options'] if option['name'] == 'book'), None)
+    language = next((option['value'] for option in interaction.data['options'] if option['name'] == 'language'), None)
     chapter = next((option['value'] for option in interaction.data['options'] if option['name'] == 'chapter'), None)
-    if book_title and version and chapter and book_title in texts and version in texts[book_title]['versions'] and chapter in texts[book_title]['versions'][version]['verses']:
-        verses = texts[book_title]['versions'][version]['verses'][chapter].keys()
+    if book and language and chapter and book in texts and language in texts[book]['versions'] and str(chapter) in texts[book]['versions'][language]['verses']:
+        verses = texts[book]['versions'][language]['verses'][str(chapter)].keys()
         return [Choice(name=verse, value=int(verse)) for verse in verses if str(current) in str(verse)]
     return []
 
